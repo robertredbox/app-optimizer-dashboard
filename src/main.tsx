712137c5932bd903a06cc6e1a1687cce9f1956e5
robertredbox <robert@redboxmobile.com>
2025-03-13 14:09:51 +0000

    Update main.tsx to initialize the AppTweak wrappers

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from 'react-dom/client'
+import React from 'react';
 import App from './App.tsx'
 import './index.css'
+import { initializeAppTweakWrappers } from './utils/appTweakWrapper';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Initialize AppTweak MCP wrappers
+initializeAppTweakWrappers();
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
